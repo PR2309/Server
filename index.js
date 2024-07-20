11339fdf15d017a64f7851c92a74d9b6e9a0ab60
PR2309 <piyushanand2309@gmail.com>
2024-07-20 13:57:56 +0530

    commit testing

--- index.js
+++ index.js
@@ -1,44 +1,5 @@
-// const express = require('express');
-// const cors = require('cors');
-// require('dotenv').config();
-// const router = require('./routes/index')
-
-// const connectDB = require('./models/db');
-// const cookieParser = require('cookie-parser');
-
-// const app = express();
-
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true
-// }));
-
-// app.use(express.json());
-// app.use(cookieParser());
-
-// const PORT = process.env.PORT || 8080;
-
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: "Server running at " + PORT
-//     });
-// });
-
-// app.use('/api',router);
-
-
-
-
-// connectDB().then(() => {
-//     app.listen(PORT, () => {
-//         console.log("Server running at " + PORT);
-//     });
-// }).catch((error) => {
-//     console.error("Failed to connect to the database:", error);
-// });
-
-
-
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -51,7 +12,7 @@ const path = require('path');
 
 const app = express();
 
-// Initialize Google Generative AI with the API key from your environment variables
+
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY); // Use environment variable for API key
 
 app.use(bodyParser.json());
@@ -65,10 +26,10 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-// const PORT = process.env.PORT || 8080;
-const PORT = process.env.PORT || "https://sparkv-server.onrender.com/";
+const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || "https://sparkv-server.onrender.com/";
 
-// Function to run the AI model
+
 async function run(name, problem) {
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const prompt = `You are an assistant for a Roadmap Website named SparkV. I am a user named ${name} and I want you to write a roadmap for me if ${problem}`;
@@ -85,7 +46,7 @@ async function run(name, problem) {
     }
 }
 
-// API route for generating AI responses
+
 app.use('/api', router);
 app.post('/ai/ans', async (req, res) => {
     const { name, problem } = req.body;
@@ -98,7 +59,6 @@ app.post('/ai/ans', async (req, res) => {
     }
 });
 
-// Connect to the database and start the server
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server running at " + PORT);
