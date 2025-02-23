5dab8ff3440f8b74aa50a4f4f8c973b375594b1a
PR2309 <piyushanand2309@gmail.com>
2025-02-23 16:09:13 +0530

    commit

--- models/db.js
+++ models/db.js
@@ -5,7 +5,8 @@ async function connectDB() {
     try {
         await mongoose.connect(process.env.MONGO_CONN, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000,
         });
         console.log("MongoDB Connected Successfully");
     } catch (error) {
